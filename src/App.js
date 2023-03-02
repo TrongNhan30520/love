1ec3b4ec5d7dfecc9d7a304b600f1c1bbb895043
TrongNhan30520 <trongnhan30520@gmail.com>
2023-03-03 00:58:56 +0700

    push code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useState } from "react";
+import Heart from "./pages/Heart";
+import HeartAndName from "./pages/HeartAndName";
+import LightHeart from "./pages/LightHeart";
+import HeartBlom from "./pages/HeartBlom";
+import HeartImage from "./pages/HeartImage";
+
+import HeartIcon from "./assets/heart-pulse-solid.svg";
 
 function App() {
+  const [step, setStep] = useState(0);
+  const handleClick = (e) => {
+    setStep(step + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(step === 0 || step === 1) && (
+        <div>
+          <button
+            onClick={handleClick}
+            style={{
+              position: "fixed",
+              display: "flex",
+              top: "40px",
+              left: "40px",
+              zIndex: 1,
+              backgroundColor: "transparent",
+              borderRadius: "25px",
+              color: "pink",
+              alignItems: "center",
+            }}
+          >
+            click=>
+            <img
+              style={{
+                width: "30px",
+                backgroundColor: "red",
+                borderRadius: "5px",
+              }}
+              src={HeartIcon}
+            />
+          </button>
+        </div>
+      )}
+      {step === 0 ? <Heart /> : step === 1 ? <HeartAndName /> : <HeartImage />}
     </div>
   );
 }
